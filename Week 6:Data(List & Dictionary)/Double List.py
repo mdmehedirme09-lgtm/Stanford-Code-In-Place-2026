1415ba3e7b0af08aa494da6c8a6b39a402ba407b
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-05-29 08:45:07 +0600

    Create Double List.py

---
+++ Week 6:Data(List & Dictionary)/Double List.py
@@ -0,0 +1,19 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+def main():
+    numbers = [1, 2, 3, 4]  # Creates a list of numbers
+
+    for i in range(len(numbers)):
+        numbers[i]=numbers[i]*2
+
+    print(numbers) # This should print the doubled list
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
