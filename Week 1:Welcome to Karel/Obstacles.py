0a72a40eab4df49f039f5703171f0b16ba381ee8
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-05-31 09:32:51 +0600

    Create Obstacles.py

---
+++ Week 1:Welcome to Karel/Obstacles.py
@@ -0,0 +1,50 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to
+solve the problem. You can click the blue show solution button on the left to see
+the answer if you get too stuck or want to check your work!
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """
+    Karel starts facing East in the bottom left corner of the world and ends facing East in the bottom right corner of the world.
+    """
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+
+
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
