7d4fe3572357edf269fa57b05daec9e67236b527
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-05-31 08:30:30 +0600

    Create Baby Vocab.py

---
+++ Week 6:Data(List & Dictionary)/Baby Vocab.py
@@ -0,0 +1,54 @@
+def main():
+    words = load_words_from_file("words.txt")
+    # TODO: your code here :)
+    counts = {}
+
+    # Count occurrences of each word
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    # Print histogram
+    for word in counts:
+        print_histogram_bar(word, counts[word])
+
+def print_histogram_bar(word, count):
+    """
+    Prints one bar in the histogram.
+    
+    Uses formatted strings to do so. The 
+        {word : <8}
+    adds white space after a string to make
+    the string take up 8 total characters of space.
+    This makes all of our words on the left of the 
+    histogram line up nicely. On the other end,
+        {'x' * count}
+    takes the 'x' string and duplicates it by 'count'
+    number of times. So 'x' * 5 would be 'xxxxx'.
+    
+    Calling print_histogram_bar("mom", 7) would print:
+        mom     : xxxxxxx
+    """
+    print(f"{word : <8}: {'x' * count}")
+
+def load_words_from_file(filepath):
+    """
+    Loads words from a file into a list and returns it.
+    We assume the file to have one word per line.
+    Returns a list of strings. You should not modify this
+    function.
+    """
+    words = []
+    with open(filepath, 'r') as file_reader:
+        for line in file_reader.readlines():
+            cleaned_line = line.strip()
+            if cleaned_line != '':
+                words.append(cleaned_line)
+    
+    return words
+
+
+if __name__ == '__main__':
+    main()
