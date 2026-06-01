28919bcff00917095e3fbe4e4749d644fa15ff3b
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-06-01 23:23:05 +0600

    Create Dog years.py

---
+++ Week 3: Intro to Python/Dog years.py
@@ -0,0 +1,11 @@
+# Each year for a human is like 7.18 years for a dog
+DOG_YEARS_MULTIPLIER = 7.18  
+
+def main():
+    age=float(input("Enter an age in calendar years: "))
+    print(f"That's {age*7.18} in dog years!")  # Delete this line and write your code here! :)
+
+
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
