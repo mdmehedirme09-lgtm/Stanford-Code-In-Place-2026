0b768b34c3f7c66a6325b0a872e13c5055478ff6
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-06-01 23:21:49 +0600

    Create How old are they?.py

---
+++ Week 3: Intro to Python/How old are they?.py
@@ -0,0 +1,30 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+def main():
+    age1=21
+    age2=21+6
+    age3=age2+20
+    age4=age1+age3
+    age5=age3
+
+
+    print("Anton is",age1)
+    
+    print("Beth is",age2)
+    print("Chen is",age3)
+    print("Drew is",age4)
+    print("Ethan is",age5)
+    
+    
+    
+
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
