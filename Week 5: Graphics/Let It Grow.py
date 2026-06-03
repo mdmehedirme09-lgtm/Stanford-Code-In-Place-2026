6f15873b44463f56a38f07f3b8b3c50986ee9787
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-06-03 20:12:12 +0600

    Create Let It Grow.py

---
+++ Week 5: Graphics/Let It Grow.py
@@ -0,0 +1,108 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+from graphics import Canvas
+import time
+    
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+DELAY = 1/3
+GROWTH_FACTOR = 10
+
+def grow_leaves(canvas, leaves):
+    # 1. Store the inforleavesmation about the shape from our canvas
+    left_x, top_y, right_x, bottom_y = get_corner_coordinates(canvas, leaves)
+    
+    # 2. Delete the original shape from the canvas
+    canvas.delete(leaves)
+    
+    # 3. Remake the object with the transformations (growing it)
+    new_leaf = canvas.create_oval(
+        left_x - GROWTH_FACTOR,
+        top_y - GROWTH_FACTOR,
+        right_x + GROWTH_FACTOR,
+        bottom_y + GROWTH_FACTOR,
+        color="green"
+    )
+    
+    return new_leaf
+
+################### No need to edit code beyond this point :) ###################
+
+def get_corner_coordinates(canvas, canvas_obj):
+    """
+    Takes a canvas object and returns the coordinates as a tuple in the form of
+    (left_x, top_y, right_x, bottom_y).
+    """
+    
+    left_x, top_y = canvas.coords(canvas_obj)
+    right_x = left_x + canvas.get_object_width(canvas_obj)
+    bottom_y = top_y + canvas.get_object_height(canvas_obj)
+    
+    return left_x, top_y, right_x, bottom_y
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    leaves = make_tree(canvas)
+    grow(canvas, leaves)
+
+def make_tree(canvas):
+    canvas.create_rectangle(CANVAS_WIDTH/2 - 20,
+                            CANVAS_HEIGHT/4 + 20,
+                            CANVAS_WIDTH/2 + 20,
+                            CANVAS_HEIGHT,
+                            color="#402F1D")
+    
+    canvas.create_line(CANVAS_WIDTH/2 - 100,
+                       CANVAS_HEIGHT/4 + 60,
+                       CANVAS_WIDTH/2 + 20,
+                       CANVAS_HEIGHT/4 + 80,
+                       color="#402F1D")
+    
+    canvas.create_line(CANVAS_WIDTH/2 + 80,
+                       CANVAS_HEIGHT/4 + 120,
+                       CANVAS_WIDTH/2 + 20,
+                       CANVAS_HEIGHT/4 + 130,
+                       color="#402F1D")
+    
+    leaves = []
+    
+    leaves.append(
+        canvas.create_oval(CANVAS_WIDTH/2 - 20,
+                           CANVAS_HEIGHT/4 - 20,
+                           CANVAS_WIDTH/2 + 20,
+                           CANVAS_HEIGHT/4 + 20,
+                           color="green")
+    )
+    
+    leaves.append(                   
+        canvas.create_oval(CANVAS_WIDTH/2 - 100,
+                           CANVAS_HEIGHT/4 + 40,
+                           CANVAS_WIDTH/2 - 60,
+                           CANVAS_HEIGHT/4 + 80,
+                           color="green")
+    )
+    
+    leaves.append(
+        canvas.create_oval(CANVAS_WIDTH/2 + 80,
+                           CANVAS_HEIGHT/4 + 100,
+                           CANVAS_WIDTH/2 + 120,
+                           CANVAS_HEIGHT/4 + 140,
+                           color="green")
+    )
+    
+    return leaves                 
+
+def grow(canvas, leaves_list):
+    for _ in range(8):
+        for i in range(len(leaves_list)):
+            leaves = leaves_list[i]
+            leaves_list[i] = grow_leaves(canvas, leaves)
+        
+        time.sleep(DELAY)
+
+if __name__ == '__main__':
+    main()
