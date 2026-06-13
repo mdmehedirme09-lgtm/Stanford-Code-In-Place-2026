e94180bfef3489bda36d0422e9754d88a7a4fff2
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-06-13 18:50:57 +0600

    Update RoboMouse: Intelligent Maze Explorer.py

--- Final Project/RoboMouse: Intelligent Maze Explorer.py
+++ Final Project/RoboMouse: Intelligent Maze Explorer.py
@@ -2,8 +2,6 @@ from graphics import Canvas
 import random
 import time
 
-
-
 ROWS = 21
 COLS = 21
 CELL_SIZE = 25
@@ -11,355 +9,219 @@ CELL_SIZE = 25
 CANVAS_WIDTH = COLS * CELL_SIZE
 CANVAS_HEIGHT = ROWS * CELL_SIZE
 
+#create empty maze
 
-# ------------------------
-# CREATE EMPTY MAZE
-# ------------------------
-
-def create_empty_maze():
-    maze = []
-
+def empty_maze():
+    
+    '''
+     Lets create an empty list &
+     later we append row to this list &
+     this become 2d array with rows & columns
+     '''
+    maze=[]#empty list
     for r in range(ROWS):
-        row = []
-
+        row=[]
         for c in range(COLS):
-            row.append('#')
-
+            row.append("#")
         maze.append(row)
 
     return maze
 
-
-# ------------------------
-# DFS MAZE GENERATOR
-# ------------------------
-
-def dfs(maze, row, col):
-
-    maze[row][col] = '.'
-
-    directions = [
-        (-2, 0),
-        (2, 0),
-        (0, -2),
-        (0, 2)
-    ]
-
-    random.shuffle(directions)
-
-    for dr, dc in directions:
-
-        nr = row + dr
-        nc = col + dc
-
-        if 1 <= nr < ROWS - 1 and 1 <= nc < COLS - 1:
-
-            if maze[nr][nc] == '#':
-
-                maze[row + dr // 2][col + dc // 2] = '.'
-
-                dfs(maze, nr, nc)
+#this function creates a random maze every run
+def dfs_maze(maze,row,col):
+    maze[row][col]='.'
+    direction=[ (2,0), #2 step right
+                (0,2), #2 step down
+                (-2,0),#2 step left
+                (0,-2)#2 step up
+              ]
+    #Randomly shuffling the direction every time when the programme runs
+    random.shuffle(direction)
+    for r,c in direction:
+        new_row=row+r
+        new_col=col+c
+        if 1<=new_row<=ROWS-2 and 1<=new_col<=COLS-2:#check the cell is inside the boundary
+            if maze[new_row][new_col]=="#":#check while the cell is unvisited
+                maze[row+r//2][col+c//2]='.'#clear the wall
+                dfs_maze(maze,new_row,new_col)#Reccursive call to the function
 
 
-# ------------------------
-# ADD LOOPS
-# ------------------------
+#we add afunction that the mouse faces multiple paths 
+def multiple_paths(maze,chance=0.15):
+    for r in range(1,ROWS-1):
+        for c in range(1,COLS-1):
+            if maze[r][c]=="#":
+                if random.random()<chance:
+                    maze[r][c]='.'#
 
-def add_loops(maze, chance=0.15):
+#we draw the maze using graphics library
 
-    for r in range(1, ROWS - 1):
-        for c in range(1, COLS - 1):
+def draw_maze(canvas,maze):
+    for r in range(ROWS):
+        for c in range(COLS):
+            x_start=c*CELL_SIZE
+            y_start=r*CELL_SIZE
+            x_end=x_start+CELL_SIZE
+            y_end=y_start+CELL_SIZE
 
-            if maze[r][c] == '#':
-
-                if random.random() < chance:
-
-                    maze[r][c] = '.'
-
-
-# ------------------------
-# DRAW MAZE
-# ------------------------
-
-def draw_maze(canvas, maze):
-
-    for row in range(ROWS):
-
-        for col in range(COLS):
-
-            x1 = col * CELL_SIZE
-            y1 = row * CELL_SIZE
-
-            x2 = x1 + CELL_SIZE
-            y2 = y1 + CELL_SIZE
-
-            if maze[row][col] == '#':
-                color = "black"
-
-            elif maze[row][col] == 'S':
-                color = "yellow"
-
-            elif maze[row][col] == 'G':
-                color = "green"
-
+            if maze[r][c]=="#":
+                color="Black"
+            elif maze[r][c]=="S":
+                color="Yellow"
+            elif maze[r][c]=="G":
+                color="Green"
             else:
-                color = "white"
+                color="White"
 
-            canvas.create_rectangle(
-                x1,
-                y1,
-                x2,
-                y2,
-                color
+            canvas.create_rectangle(x_start,
+                                    y_start,
+                                    x_end,
+                                    y_end,
+                                    color
             )
 
 
-# ------------------------
-# DRAW MOUSE
-# ------------------------
-
 def draw_mouse(canvas):
-
-    size = CELL_SIZE - 10
-
-    mouse = canvas.create_oval(
-        0,
-        0,
-        size,
-        size,
-        "blue"
-    )
+    size=CELL_SIZE-10
+    mouse=canvas.create_oval(0,0,size,size,color="blue")
 
     return mouse
 
+'''
+we need to convert the coordinates to
+pixel because canvas uses pixel coordinates
+'''
+def make_pixel(row,col):
+    x=col*CELL_SIZE+5
+    y=row*CELL_SIZE+5
 
-# ------------------------
-# GRID TO PIXEL
-# ------------------------
+    return x,y 
 
-def cell_to_pixel(row, col):
+'''
+We implement flood fill Algorithm
+in order to find the distance of 
+every cell from the goal cell
+'''
 
-    x = col * CELL_SIZE + 5
-    y = row * CELL_SIZE + 5
-
-    return x, y
-
-
-# ------------------------
-# FLOOD FILL
-# ------------------------
-
-def flood_fill(maze, goal):
-
+def flood_fill(maze,goal):
     values = []
-
     for r in range(ROWS):
-
         row = []
-
         for c in range(COLS):
-
             if maze[r][c] == '#':
                 row.append(-1)
             else:
                 row.append(999)
-
         values.append(row)
-
     goal_row, goal_col = goal
-
     values[goal_row][goal_col] = 0
-
     changed = True
 
     while changed:
-
         changed = False
-
         for r in range(ROWS):
-
             for c in range(COLS):
-
                 if values[r][c] == -1:
                     continue
-
                 directions = [
                     (-1, 0),
                     (1, 0),
                     (0, -1),
                     (0, 1)
                 ]
-
                 for dr, dc in directions:
-
                     nr = r + dr
                     nc = c + dc
-
                     if 0 <= nr < ROWS and 0 <= nc < COLS:
-
                         if values[nr][nc] != -1:
-
                             if values[r][c] > values[nr][nc] + 1:
-
                                 values[r][c] = values[nr][nc] + 1
-
                                 changed = True
 
     return values
 
-
-# ------------------------
-# GET FLOOD PATH
-# ------------------------
-
-def get_flood_path(values, start, goal):
-
-    path = [start]
-
-    row, col = start
-
-    while (row, col) != goal:
-
-        choices = []
-
-        best_value = values[row][col]
-
+#this function helps mouse to moves along the flood fill path
+def get_flood_fill_path(value,start,goal):
+    path=[start]
+    r,c=start
+    while(r,c)!=goal:
+        choices=[]
+        best_value=value[r][c]
         directions = [
             (-1, 0),
             (1, 0),
             (0, -1),
             (0, 1)
-        ]
-
+            ]
         for dr, dc in directions:
-
-            nr = row + dr
-            nc = col + dc
-
+            nr = r + dr
+            nc = c + dc
             if 0 <= nr < ROWS and 0 <= nc < COLS:
-
-                if values[nr][nc] != -1:
-
-                    if values[nr][nc] < best_value:
-
-                        best_value = values[nr][nc]
+                if value[nr][nc] != -1:
+                    if value[nr][nc] < best_value:
+                        best_value = value[nr][nc]
                         choices = [(nr, nc)]
-
-                    elif values[nr][nc] == best_value:
+                    elif value[nr][nc] == best_value:
                         choices.append((nr, nc))
-
-        row, col = random.choice(choices)
-
-        path.append((row, col))
-
+    
+        r,c = random.choice(choices)
+        path.append((r, c))
     return path
 
-
-# ------------------------
-# DRAW FLOOD VALUES
-# ------------------------
+#draw values to each cell according to flood fill algorithm that the mouse choice for move
 def draw_values(canvas, values):
-
     for r in range(ROWS):
-
         for c in range(COLS):
-
             if values[r][c] != -1:#-1 refers to presence of wall
-
                 canvas.create_text(
                     c * CELL_SIZE + CELL_SIZE / 2,
                     r * CELL_SIZE + CELL_SIZE / 2,
                     str(values[r][c]),
-                    color="purple"
+                    color="maroon"
                 )
 
-
-# ------------------------
-# ANIMATE MOUSE
-# ------------------------
-
+#We use animation to see the mouse movement
 def animate_mouse(canvas, mouse, path):
-
     current_row, current_col = path[0]
-
-    x, y = cell_to_pixel(current_row, current_col)
-
+    x, y = make_pixel(current_row, current_col)
     canvas.moveto(mouse, x, y)
-
     time.sleep(0.5)
 
     for row, col in path[1:]:
-
         dx = (col - current_col) * CELL_SIZE
         dy = (row - current_row) * CELL_SIZE
-
         canvas.move(mouse, dx, dy)
-
         current_row = row
         current_col = col
 
         time.sleep(0.08)
 
 
-# ------------------------
-# MAIN
-# ------------------------
-
+#finally the main function where we call the above function
 def main():
-
-    canvas = Canvas(
-        CANVAS_WIDTH,
-        CANVAS_HEIGHT
-    )
-
-    maze = create_empty_maze()
-
-    dfs(maze, 1, 1)
-
+    canvas = Canvas(CANVAS_WIDTH,CANVAS_HEIGHT)
+    maze = empty_maze()
+    dfs_maze(maze, 1, 1)
     # ADD EXTRA PATHS
-    add_loops(maze, 0.15)
+    multiple_paths(maze, 0.15)
 
     start = (1, 1)
     goal = (ROWS - 2, COLS - 2)
-
     start_row, start_col = start
     goal_row, goal_col = goal
 
     maze[start_row][start_col] = 'S'
     maze[goal_row][goal_col] = 'G'
 
-    values = flood_fill(
-        maze,
-        goal
-    )
-
-    draw_maze(
-        canvas,
-        maze
-    )
-
-    draw_values(
-        canvas,
-        values
-    )
-
+    values = flood_fill(maze,goal)
+    draw_maze( canvas,maze)
+    draw_values(canvas,values)
     mouse = draw_mouse(canvas)
-
-    path = get_flood_path(
-        values,
-        start,
-        goal
-    )
-
-    animate_mouse(
-        canvas,
-        mouse,
-        path
-    )
+    path = get_flood_fill_path(values,start,goal)
+    animate_mouse(canvas,mouse,path)
 
     print("Maze Solved!")
     
-
 
 if __name__ == '__main__':
     main()
